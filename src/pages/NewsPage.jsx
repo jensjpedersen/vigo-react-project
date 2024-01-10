69fce80a8ed4b35c1d6be266bf30c9bd66af00f9
Jens Junker Pedersen <jens.junker.pedersen@gmail.com>
2024-01-10 09:12:38 +0100

    feat(NewsPage): added style template.

--- src/pages/NewsPage.jsx
+++ src/pages/NewsPage.jsx
@@ -1,13 +1,28 @@
 
 import styled from 'styled-components';
+import { useEffect, useState } from 'react';
 
 
 
+
+
+
+
+
+
+
+const StyledNewsPage = styled.div`
+    background-color: #333;
+
+`
 
 export default function NewsPage() {
 
     return (
-        <h1>News page</h1>
+        <StyledNewsPage>
+            <h1>News page</h1>
+            <p>Les sisten inlegg</p>
+        </StyledNewsPage>
     )
 
 }

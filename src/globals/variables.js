148732161ab4f99284744216f0ceade1a6a85d36
Jens Junker Pedersen <jens.junker.pedersen@gmail.com>
2024-01-10 14:42:29 +0100

    feat: added object with colors.

---
+++ src/globals/variables.js
@@ -0,0 +1,17 @@
+export default {
+  colors: {
+    dark1: 'hsl(60, 3%, 15%)', 
+    dark2: 'hsl(0, 0%, 26%)', 
+    dark3: 'hsl(220, 2%, 33%)', 
+
+    light1: 'hsl(0, 0%, 53%)',  
+    light2: 'hsl(0, 0%, 90%)', 
+
+    accent1: 'hsl(190, 67%, 12%)',
+    accent2: 'hsl(190, 48%, 24%)',
+    accent3: 'hsl(190, 32%, 93%)',
+  }
+
+
+}
+

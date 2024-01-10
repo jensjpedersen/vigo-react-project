fe04ff9ef5264ca601e734c4acb1afc048ecdea2
Jens Junker Pedersen <jens.junker.pedersen@gmail.com>
2024-01-10 15:04:13 +0100

    feat: added variables.

--- src/globals/variables.js
+++ src/globals/variables.js
@@ -1,8 +1,10 @@
 export default {
   colors: {
-    dark1: 'hsl(60, 3%, 15%)', 
-    dark2: 'hsl(0, 0%, 26%)', 
-    dark3: 'hsl(220, 2%, 33%)', 
+
+    dark1: 'hsl(0, 0%, 11%)',
+    dark2: 'hsl(60, 3%, 15%)', 
+    dark3: 'hsl(0, 0%, 26%)', 
+    dark4: 'hsl(220, 2%, 33%)', 
 
     light1: 'hsl(0, 0%, 53%)',  
     light2: 'hsl(0, 0%, 90%)', 
@@ -10,7 +12,14 @@ export default {
     accent1: 'hsl(190, 67%, 12%)',
     accent2: 'hsl(190, 48%, 24%)',
     accent3: 'hsl(190, 32%, 93%)',
+  },
+
+
+  dimesions: {
+    headerHeight: '60px'
   }
+
+
 
 
 }

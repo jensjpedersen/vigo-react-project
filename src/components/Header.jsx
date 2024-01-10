1e27efa90352cdd52087ea366ca022662a02346d
Jens Junker Pedersen <jens.junker.pedersen@gmail.com>
2024-01-10 14:57:28 +0100

    feat: styled header.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,47 @@
 
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import variables from '../globals/variables'
 
 
 const StyledHeader = styled.header`
-    background-color: #333;
+    background-color: ${variables.colors.dark1};
+    height: ${variables.dimesions.headerHeight};
+
+    display: flex; 
+    justify-content: space-between; 
+
+
+    nav {
+        display: flex;
+        align-items: center;
+        margin: 0 4rem;
+    }
+
 
     ul {
         display: flex; 
-        gap: 1rem;
-
-        li {
-            list-style: none;
-        }
+        gap: 2rem;
 
     }
+
+    li {
+      list-style: none;
+    }
+
+
+    a {
+      text-decoration: none;
+      color: ${variables.colors.light2}; 
+      font-size: 1.2rem;
+      font-weight: 600; 
+
+      &:hover {
+        color: red; 
+      }
+    }
+
+
 `
 
 
@@ -22,8 +49,14 @@ export default function Header() {
     return (
         <StyledHeader>
             <nav>
+                <Link to="/">
+                    Home
+                </Link> 
+            </nav>
+
+
+            <nav>
                 <ul>
-                    <li> <Link to="/">Home</Link> </li>
                     <li> <Link to="/writers">Writers</Link> </li>
                     <li> <Link to="/news">Nyheter</Link> </li>
                 </ul>

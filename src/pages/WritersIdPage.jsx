e6ba19080c80252a30fed073718400053893a652
Jens Junker Pedersen <jens.junker.pedersen@gmail.com>
2024-01-11 16:04:36 +0100

    feat(user posts): place user specific posts when profile is clicked.

--- src/pages/WritersIdPage.jsx
+++ src/pages/WritersIdPage.jsx
@@ -1,25 +1,38 @@
 
-
-
-
+import { useState, useEffect } from 'react';
 import { useParams, useLocation } from "react-router-dom";
 import styled from 'styled-components';
+import { fetchData } from '../utils/api.js';
+import Posts from '../components/Posts.jsx';
 
 
 const StyledWritersIdPage = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
     margin-top: 100px;
+    
+    h2 {
+        margin-bottom: 50px;
+
+    }
+
 `
 
-
 export default function WritersIdPage() {
+    const id = useParams().id;
+    let [ postsData, setPostsData ] = useState(null);
 
-    console.log(useLocation().state);
+    useEffect(() => {
+        console.log('useEffect');
+        const url = `https://jsonplaceholder.typicode.com/posts/?userId=${id}`; 
+        fetchData(url).then(data => setPostsData(data));
+    }, [])
 
     return (
         <StyledWritersIdPage>
             <h2>Artikler av {useLocation().state}</h2>
-
-
+            { postsData && <Posts posts={postsData}/> }
         </StyledWritersIdPage>
     )
 }

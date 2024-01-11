2876879718685020902d3375335234e2c033357d
Jens Junker Pedersen <jens.junker.pedersen@gmail.com>
2024-01-11 11:51:13 +0100

    feat: footer text.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,30 @@
 
 import styled from 'styled-components';
+import variables from "../globals/variables";
 
 
+// 698
 
-const StyledFooter = styled.div`
+const StyledFooter = styled.footer`
+
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+
+    background-color: ${variables.colors.accent1};
+    height: ${variables.dimesions.footerHeight};
+    min-width: 700px;
 `
 
 
 export default function Footer() {
     return (
         <StyledFooter>
-            <h1>Footer</h1>
+        <p>&copy; 2024 Your Website. All rights reserved.</p>
+        <p>Contact: info@example.com</p>
         </StyledFooter>
     )
 }

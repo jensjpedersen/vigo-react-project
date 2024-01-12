6665e81a1b56d7358fa61b396b609f400c973960
Jens Junker Pedersen <jens.junker.pedersen@gmail.com>
2024-01-12 11:46:10 +0100

    feat: conditional footer margin styling.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,28 +1,29 @@
 
 import styled from 'styled-components';
 import variables from "../globals/variables";
+import { useLocation } from 'react-router-dom';
 
-
-// 698
 
 const StyledFooter = styled.footer`
-
 
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
 
-
     background-color: ${variables.colors.accent1};
     height: ${variables.dimesions.footerHeight};
     min-width: 700px;
+
+    margin-top: ${props => props.isMainPage ? '0' : '30px'};
 `
 
 
 export default function Footer() {
+    const isMainPage = useLocation().pathname === '/';
+
     return (
-        <StyledFooter>
+        <StyledFooter isMainPage={isMainPage}>
         <p>&copy; 2024 Your Website. All rights reserved.</p>
         <p>Contact: info@example.com</p>
         </StyledFooter>
